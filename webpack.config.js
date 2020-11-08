f571c056255b15b121ae9a0a2586816886210eed
toshi-toma <toshitoma1217@gmail.com>
2020-11-08 21:11:31 +0900

    add plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,25 @@
-const path = require('path');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 /** @type import('webpack').Configuration */
 module.exports = {
   entry: path.resolve(__dirname, "src/index.js"),
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: "main.js"
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
   },
   module: {
     rules: [
       {
         test: /\.ts$/,
-        use: "ts-loader"
-      }
-    ]
+        use: "ts-loader",
+      },
+    ],
   },
   resolve: {
-    extensions: ['.js', '.ts']
-  }
-}
+    extensions: [".js", ".ts"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin()
+  ]
+};
