59c8cd972ade623f35c73d6953f0675fc19ffd0d
toshi-toma <toshitoma1217@gmail.com>
2020-11-11 00:58:25 +0900

    add webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,30 @@
 const path = require("path");
+const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 /** @type import('webpack').Configuration */
-module.exports = {
-  entry: path.resolve(__dirname, "src/index.js"),
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "main.js",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        use: "ts-loader",
-      },
-    ],
-  },
-  resolve: {
-    extensions: [".js", ".ts"],
-  },
-  plugins: [
-    new HtmlWebpackPlugin()
-  ]
+module.exports = () => {
+  return {
+    entry: path.resolve(__dirname, "src/index.js"),
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "[chunkhash].js",
+    },
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          use: "ts-loader",
+        },
+      ],
+    },
+    resolve: {
+      extensions: [".js", ".ts"],
+    },
+    plugins: [new HtmlWebpackPlugin(), new webpack.ProgressPlugin()],
+    devServer: {
+      contentBase: path.resolve(__dirname, 'dist'),
+      open: true
+    }
+  };
 };
