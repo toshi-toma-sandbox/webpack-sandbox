269562581ac5a3cc843e6eac20e836641dc929f3
toshi-toma <toshitoma1217@gmail.com>
2020-11-14 11:06:03 +0900

    add bundle analyzer

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,15 @@
 const path = require("path");
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+const mode = process.env.NODE_ENV || "development";
 
 /** @type import('webpack').Configuration */
 module.exports = () => {
+  const devPlugins = mode === "development" ? [new BundleAnalyzerPlugin()] : [];
   return {
+    mode, 
     entry: path.resolve(__dirname, "src/index.js"),
     output: {
       path: path.resolve(__dirname, "dist"),
@@ -37,7 +42,7 @@ module.exports = () => {
     resolve: {
       extensions: [".js", ".ts"],
     },
-    plugins: [new HtmlWebpackPlugin(), new webpack.ProgressPlugin()],
+    plugins: [new HtmlWebpackPlugin(), new webpack.ProgressPlugin(), ...devPlugins],
     devServer: {
       contentBase: path.resolve(__dirname, "dist"),
       open: true,
